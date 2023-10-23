b579d0462a6ca189bc91a014d44db14c5e00bf30
Line Ibo <ibo.line@hotmail.fr>
2023-10-23 09:12:50 +0200

    la section projects est en cours de création, modifications de la saction about

--- script/script.js
+++ script/script.js
@@ -1,13 +1,1 @@
-const navBar = document.querySelector(".navbar-flex");
 
-window.addEventListener("scroll", () => {
-  scrollPosition = window.scrollY;
-
-  if (scrollPosition > 20) {
-    navBar.style.paddingTop = "10px";
-    navBar.style.paddingBottom = "10px";
-  } else {
-    navBar.style.paddingTop = "30px";
-    navBar.style.paddingBottom = "30px";
-  }
-});
