c3d7e1dd4d0051c299f0e45f2cd421f6d226b786
Line Ibo <ibo.line@hotmail.fr>
2023-10-23 18:54:29 +0200

    ajout de parallax sur les images de fond

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
+const parallaxImage = document.querySelector(".parallax_img");
+const parallaxSkillsImage = document.querySelector(".parallax-skills_img");
 
+const title = document.querySelector(".title");
+const titleContent = title.querySelectorAll("*");
+const lineImage = title.querySelector(".line_image");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  parallaxImage.style.transform = `translateY(${scrollY * 0.4}px)`;
+  parallaxSkillsImage.style.transform = `translateY(-${scrollY * 0.4}px)`;
+
+  titleContent.forEach((elements) => {
+    elements.style.transform = `translateY(${scrollY * 0.5}px)`;
+  });
+});
+
+window.addEventListener("load", () => {
+  lineImage.style.width = "450px";
+});
