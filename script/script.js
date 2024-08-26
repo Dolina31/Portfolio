fa695461735c2dad2d1793d837d2e006e295ad8d
Line Ibo <ibo.line@hotmail.fr>
2024-08-27 00:45:46 +0200

    changement de l'image pour la section about + modifications des tailles des differentes sections

--- script/script.js
+++ script/script.js
@@ -8,11 +8,13 @@ const navBarMenu = document.querySelector(".navbar_menu");
 const navBarMenuLink = document.querySelectorAll(".navbar_menu_link");
 const title = document.querySelector(".title");
 const aboutImage = document.querySelector(".about_image");
-const aboutText = document.querySelector(".about_text");
+const aboutText = document.querySelector(".about_text p");
 const skillsImageWrapper = document.querySelectorAll(".skills_images_wrapper");
 const skillsContent = document.querySelector(".skills_content");
 const contactSection = document.querySelector(".contact");
 const contactLinks = document.querySelectorAll(".contact_link");
+
+console.log(aboutText);
 
 window.addEventListener("load", () => {
   navBar.style.opacity = "1";
@@ -27,62 +29,6 @@ window.addEventListener("load", () => {
 
   title.style.width = "100%";
   title.style.opacity = "1";
-});
-
-window.addEventListener("scroll", () => {
-  parallaxImage.style.transform = `translateY(${scrollY * 0.4}px)`;
-  parallaxSkillsImage.style.transform = `translateY(-${scrollY * 0.4}px)`;
-  returnHomeIcon.style.visibility = "visible";
-});
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      switch (entry.target) {
-        case aboutImage:
-          aboutImage.style.transform = "translateX(0)";
-          aboutImage.style.opacity = "1";
-          break;
-        case aboutText:
-          aboutText.style.transform = "translateX(0)";
-          aboutText.style.opacity = "1";
-          break;
-        case skillsContent:
-          skillsContent.style.width = "93%";
-          setTimeout(() => {
-            skillsImageWrapper.forEach((wrapper, index) => {
-              setTimeout(() => {
-                wrapper.style.transform = "translateY(0)";
-                wrapper.style.opacity = "1";
-              }, index * 100 + 100);
-            });
-          }, 100);
-          break;
-        case contactSection:
-          contactLinks.forEach((link, index) => {
-            setTimeout(() => {
-              link.style.opacity = "1";
-              link.style.transform = "translateY(0)";
-            }, index * 300 + 300);
-          });
-          break;
-      }
-    }
-  });
-});
-
-observer.observe(parallaxImage);
-observer.observe(parallaxSkillsImage);
-observer.observe(returnHomeIcon);
-observer.observe(aboutImage);
-observer.observe(aboutText);
-observer.observe(contactSection);
-skillsImageWrapper.forEach((wrapper) => {
-  observer.observe(wrapper);
-});
-observer.observe(skillsContent);
-contactLinks.forEach((link) => {
-  observer.observe(link);
 });
 
 cards.forEach((card) => {
@@ -127,4 +73,67 @@ navBarIcon.addEventListener("click", () => {
   }
 
   isNavbarVisible = !isNavbarVisible;
+});
+
+window.addEventListener("scroll", () => {
+  parallaxImage.style.transform = `translateY(${scrollY * 0.4}px)`;
+  parallaxSkillsImage.style.transform = `translateY(-${scrollY * 0.4}px)`;
+  returnHomeIcon.style.visibility = "visible";
+});
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.55,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      switch (entry.target) {
+        case aboutImage:
+          aboutImage.style.transform = "translateX(0)";
+          aboutImage.style.opacity = "1";
+          break;
+        case aboutText:
+          aboutText.style.transform = "translateY(0)";
+          aboutText.style.opacity = "1";
+          break;
+        case skillsContent:
+          skillsContent.style.width = "93%";
+          skillsContent.style.padding = "15px";
+          setTimeout(() => {
+            skillsImageWrapper.forEach((wrapper, index) => {
+              setTimeout(() => {
+                wrapper.style.transform = "translateY(0)";
+                wrapper.style.opacity = "1";
+              }, index * 100 + 100);
+            });
+          }, 100);
+          break;
+        case contactSection:
+          contactLinks.forEach((link, index) => {
+            setTimeout(() => {
+              link.style.opacity = "1";
+              link.style.transform = "translateY(0)";
+            }, index * 300 + 300);
+          });
+          break;
+      }
+    }
+  });
+}, options);
+
+observer.observe(parallaxImage);
+observer.observe(parallaxSkillsImage);
+observer.observe(returnHomeIcon);
+observer.observe(aboutImage);
+observer.observe(aboutText);
+observer.observe(contactSection);
+skillsImageWrapper.forEach((wrapper) => {
+  observer.observe(wrapper);
+});
+observer.observe(skillsContent);
+contactLinks.forEach((link) => {
+  observer.observe(link);
 });
